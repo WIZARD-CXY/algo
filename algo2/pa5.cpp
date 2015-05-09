cd95120b3fcb20db3a2384d9233085209e71ba5e
wizard <wizard_cxy@hotmail.com>
2015-05-09 15:02:06 +0800

    finish the final exam

--- algo2/pa5.cpp
+++ algo2/pa5.cpp
@@ -7,6 +7,44 @@ struct city{
 	double mx,my;
 };
 
+uint_t nextnum(uint_t x)
+{
+ 
+  uint_t rightOne;
+  uint_t nextHigherOneBit;
+  uint_t rightOnesPattern;
+ 
+  uint_t next = 0;
+ 
+  if(x)
+  {
+ 
+    // right most set bit
+    rightOne = x & -(signed long)x;
+ 
+    // reset the pattern and set next higher bit
+    // left part of x will be here
+    nextHigherOneBit = x + rightOne;
+ 
+    // nextHigherOneBit is now part [D] of the above explanation.
+ 
+    // isolate the pattern
+    rightOnesPattern = x ^ nextHigherOneBit;
+ 
+    // right adjust pattern
+    rightOnesPattern = (rightOnesPattern)/rightOne;
+ 
+    // correction factor
+    rightOnesPattern >>= 2;
+ 
+    // rightOnesPattern is now part [A] of the above explanation.
+ 
+    // integrate new pattern (Add [D] and [A])
+    next = nextHigherOneBit | rightOnesPattern;
+  }
+ 
+  return next;
+}
 int main(){
 	freopen("tsp.txt","r",stdin);
 	int n;
@@ -22,14 +60,14 @@ int main(){
 
 	}
 
-	double length[n+1][n+1];
+	double w[n+1][n+1];
 
 	for(int i=0; i<n;i++){
-		for(int j=0; j<n; j++){
+		for(int j=i+1; j<n; j++){
 			if(i==j){
-				length[i][i]=0;
+				w[i][i]=0;
 			}else{
-				length[i][j]=length[j][i]=sqrt(pow((city[i].mx-city[j].mx),2)+pow((city[i].my-city[j].my),2));
+				w[i][j]=w[j][i]=sqrt(pow(city[i].mx-city[j].mx,2)+pow(city[i].my-city[j].my,2));
 			}
 		}
 	}
